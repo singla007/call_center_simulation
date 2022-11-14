e6eceaf60d6e065925b9479b58250d8fad5c7deb
Yogesh Singla <eryogeshkumarsingla007@gmail.com>
2022-11-14 13:16:35 -0500

    Delete server.py

--- system_code/server.py
+++
@@ -1,9 +0,0 @@
-class server:
-    def __init__(self):
-        self.is_busy = False
-        self.curr_service_time = None
-        self.is_fail = False
-        self.server_recovery_time = 5
-        self.curr_service_begin = 0
-        self.curr_service_end_at = None
-        self.total_server_busy_time = 0
