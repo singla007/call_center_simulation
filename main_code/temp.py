029fdea15ba3b6857021e033b8b1e961135d5823
Yogesh Singla <eryogeshkumarsingla007@gmail.com>
2022-11-20 12:49:09 -0500

    adding server 3 and 4

--- main_code/temp.py
+++ main_code/temp.py
@@ -24,13 +24,14 @@ def call_centre():
     total_wait_time = 0
 
     # Getting data
-    upto_data = 50
-    server1_data = pd.read_csv("./processed_data/server1_data.csv").iloc[:upto_data]
-    server2_data = pd.read_csv("./processed_data/server2_data.csv").iloc[:upto_data]
-    server3_data = pd.read_csv("./processed_data/server3_data.csv").iloc[:upto_data]
-    server4_data = pd.read_csv("./processed_data/server4_data.csv").iloc[:upto_data]
-    server1_service_time = [1,1,1,1,1,1,1,1,1,1,1,1,1,1]#list(server1_data['service_length'])
-    server2_service_time = [1,1,1,1,1,1,1,1,1,1,1,1,1,1]#list(server2_data['service_length'])
+    from_data = 5
+    upto_data = 55
+    server1_data = pd.read_csv("./processed_data/server1_data.csv").iloc[from_data:upto_data]
+    server2_data = pd.read_csv("./processed_data/server2_data.csv").iloc[from_data:upto_data]
+    server3_data = pd.read_csv("./processed_data/server3_data.csv").iloc[from_data:upto_data]
+    server4_data = pd.read_csv("./processed_data/server4_data.csv").iloc[from_data:upto_data]
+    server1_service_time = list(server1_data['service_length'])
+    server2_service_time = list(server2_data['service_length'])
     server3_service_time = list(server3_data['service_length'])
     server4_service_time = list(server4_data['service_length'])
     server_chosen = list(server2_data['server_chosen'])
@@ -56,6 +57,10 @@ def call_centre():
     max_queue2_length = 0
     print(customer_arrival_time)
 
+    queue1_wait_time = 0
+    queue2_wait_time = 0
+    customers_waited_in_queue1 = 0
+    customers_waited_in_queue2 = 0
 
 
 
@@ -64,12 +69,14 @@ def call_centre():
             print("Start")
         print("custom_timer", custom_timer)
 
-
+        queue1_wait_time += len(waiting_queue1)
+        queue2_wait_time += len(waiting_queue2)
         # Customer arrival
         if custom_timer >= customer_arrival_time[customer_index]:
             waiting_queue1.append(customer_index)
             customer_index += 1
 
+        # Customer entering into server 2
         if (server1.curr_service_end + 1) == custom_timer and custom_timer!=0:
             print("Inside server 2")
             server2.curr_service_begin = custom_timer
@@ -79,26 +86,22 @@ def call_centre():
             server1.curr_service_begin = -1
             server1.curr_service_end = -1
 
+        # Customer entering into server 1
         if not server1.is_busy and not server2.is_busy and len(waiting_queue1) >= 1:
             print("Inside server 1")
             server1.curr_service_begin = custom_timer
             server1.curr_service_end = custom_timer + server1_service_time[service_index1]
             service_index1 += 1
             server1.is_busy = True
+            waiting_queue1.pop(0)
 
-
-
-
-
-
-
-
+        # Departure from server 1
         if server1.curr_service_end == custom_timer:
             print("Service 1 end")
             server1.is_busy = False
             server1.total_server_busy_time += server1.curr_service_end - server1.curr_service_begin
 
-
+        # Departure from server 2
         if server2.curr_service_end == custom_timer:
             print("Service 2 end")
             server2.is_busy = False
@@ -108,27 +111,32 @@ def call_centre():
             waiting_queue2.append(server_chosen[server_chosen_index])
             server_chosen_index +=1
 
+        # Customer entering into server 3 and 4
         if len(waiting_queue2) >=1:
-            if waiting_queue2[0] == "S3" and not server3.is_busy:
+            if len(waiting_queue2)>=1 and waiting_queue2[0] == "S3" and not server3.is_busy:
                 print("Inside server 3")
                 server3.is_busy = True
                 server3.curr_service_begin = custom_timer
                 server3.curr_service_end = custom_timer + server3_service_time[service_index3]
                 service_index3 += 1
+                waiting_queue2.pop(0)
 
-            if waiting_queue2[0] == "S4" and not server4.is_busy:
+            if len(waiting_queue2)>=1 and waiting_queue2[0] == "S4" and not server4.is_busy:
                 print("Inside server 4")
                 server4.is_busy = True
                 server4.curr_service_begin = custom_timer
                 server4.curr_service_end = custom_timer + server4_service_time[service_index4]
                 service_index4 += 1
+                waiting_queue2.pop(0)
 
+        # Departure from server 3
         if server3.curr_service_end == custom_timer:
             server3.total_server_busy_time += server3.curr_service_end - server3.curr_service_begin
             server3.is_busy = False
             server3.curr_service_begin = -1
             server3.curr_service_end = -1
 
+        # Departure from server 4
         if server4.curr_service_end == custom_timer:
             server4.total_server_busy_time += server4.curr_service_end - server4.curr_service_begin
             server4.is_busy = False
@@ -140,15 +148,33 @@ def call_centre():
                 waiting_queue2) == 0 and not server1.is_busy and not server2.is_busy and not server3.is_busy and not server4.is_busy:
             break
 
+        if max_queue1_length < len(waiting_queue1):
+            max_queue1_length = len(waiting_queue1)
+
+        if max_queue2_length < len(waiting_queue2):
+            max_queue2_length = len(waiting_queue2)
+
         custom_timer += 1
+        simulation_run_length = custom_timer
+
         print("waiting_queue1", waiting_queue1)
         print("total_server1_service_time", server1.total_server_busy_time)
         print("total_server2_service_time", server2.total_server_busy_time)
         print("total_server3_service_time", server3.total_server_busy_time)
         print("total_server4_service_time", server4.total_server_busy_time)
+        print("total simulation time", )
         print("waiting_queue2", waiting_queue2)
-    print("Max queue 1 length", max_queue1_length)
-    print("Max queue 2 length", max_queue2_length)
+        print("Max queue 1 length", max_queue1_length)
+        print("Max queue 2 length", max_queue2_length)
+        print("Idle server1 time", simulation_run_length -  server1.total_server_busy_time)
+        print("Idle server2 time", simulation_run_length - server2.total_server_busy_time)
+        print("Idle server3 time", simulation_run_length - server3.total_server_busy_time)
+        print("Idle server4 time", simulation_run_length - server4.total_server_busy_time)
+        print("Mean Interarrival time", sum(customer_interarrival_time)/len(customer_interarrival_time))
+        print("Mean service time for server 1", sum(server1_service_time)/len(server1_service_time))
+        print("Mean service time for server 2", sum(server2_service_time) / len(server2_service_time))
+        print("Mean service time for server 3", sum(server3_service_time) / len(server3_service_time))
+        print("Mean service time for server 4", sum(server4_service_time) / len(server4_service_time))
 
 
 call_centre()
